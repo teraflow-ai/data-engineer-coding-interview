b6cac4c90dfbaf73ac5ee04b19ab2c948238d10f
zipate <eddyokemwa@gmail.com>
2022-11-16 16:21:32 +0200

    a new message

--- Glue/etl.py
+++ Glue/etl.py
@@ -1,1 +1,81 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from s3_concat import S3Concat
 
+"""
+This Glue Job will process data from the Banking Group Aurora database and generate a monthly file of the moving average loan amounts.
+The results will be written to S3 in a path partitioned by Bank name, year and month.
+"""
+
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+# Create source DynamicFrames from the Glue Data Catalog tables
+src_loan = glueContext.create_dynamic_frame.from_catalog(database = "aurora", table_name = "finance_public_loans", transformation_ctx = "src_loan")
+src_account = glueContext.create_dynamic_frame.from_catalog(database = "aurora", table_name = "finance_public_account", transformation_ctx = "src_account")
+src_client = glueContext.create_dynamic_frame.from_catalog(database = "aurora", table_name = "finance_public_client", transformation_ctx = "src_client")
+src_branch = glueContext.create_dynamic_frame.from_catalog(database = "aurora", table_name = "finance_public_branch", transformation_ctx = "src_branch")
+src_bank =  glueContext.create_dynamic_frame.from_catalog(database = "aurora", table_name = "finance_public_bank", transformation_ctx = "src_bank")
+
+
+# Join the Bank and Branch
+bank_branch = src_bank.join(paths1=["idBank"], paths2=["Bank_idBank"], frame2=src_branch)
+# Join the bank_branch to Clients
+bank_client = src_client.join(paths1=["Branch_idBranch"], paths2=["idBranch"], frame2=bank_branch)
+# Join the bank_branch_clients to Account
+bank_account = src_account.join(paths1=["Client_idClient"], paths2=["idClient"], frame2=bank_client)
+
+# Get the final DynamicFrame and use ApplyMapping to eliminate unnecessary columns
+bank_loans= src_loan.join(paths1=["Account_idAccount"], paths2=["idAccount"], frame2=bank_account)
+fn_bank_loans = bank_loans.apply_mapping(
+    [
+        ("Name", "String", "BankName", "String"),
+        ("idBranch", "long", "idBranch", "long"),
+        ("Amount", "Double", "Amount", "Double"),
+        ("loan_date", "Date", "loan_date", "Date"),
+    ]
+)
+fn_bank_loans.printSchema()
+fn_bank_loans.count()
+# Calculate the moving average using spark sql
+SqlQuery = """
+select BankName, idBranch, year, month, 
+         avg(amount) over (partition by BankName, idBranch order by year, month rows between 2 preceding and current row) rolling_average
+from
+(    select BankName, idBranch,            
+             date_format(loan_date, "yyyy") year,
+             date_format(loan_date, "MM") month,
+             sum(amount) amount
+     from myDataSource 
+     group by BankName, idBranch,
+             date_format(loan_date, "yyyy"),
+             date_format(loan_date, "MM")
+)
+"""
+fn_bank_loans.toDF().createOrReplaceTempView('myDataSource')
+results = spark.sql(SqlQuery)
+#results.count()
+#results.printSchema()
+## Write Spark Dataframe results to S3
+results.count()
+results.write.format('csv') \
+             .mode("append") \
+             .partitionBy("BankName", "year", "month") \
+             .save(path="s3://aws-edokemwa-csv/test-data/")
+
+# Loop through dest to merge and rename file
+bucket = 'aws-edokemwa-csv'
+for row in results.select("BankName", "year", "month").distinct().collect():
+    path_to_concat = 'test-data/BankName='+row['BankName']+'/year='+row['year']+'/month='+row['month']+'/'
+    concatenated_file = path_to_concat+row['BankName']+'_'+row['year']+row['month']+'01.csv'
+    min_file_size = None
+    # Init the concat_job
+    concat_job = S3Concat(bucket, concatenated_file, min_file_size, content_type='application/csv')
+    concat_job.add_files(path_to_concat)
+    concat_job.concat(small_parts_threads=4)
+job.commit()
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,175 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 ##################
 # Glue Catalog   #
 ##################
+resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
+  name = "banking_group"
+}
 
-##################
-# Glue Crawler   #
-##################
+
+#################################
+# Glue Connection and Crawler   #
+#################################
+resource "aws_glue_connection" "banking_connection" {
+  connection_properties = {
+    JDBC_CONNECTION_URL = "jdbc:postgresql://aurora-pgress.cluster-cm6elvieiaiz.us-east-1.rds.amazonaws.com:5432/finance"
+    PASSWORD            = "2486Okebe."
+    USERNAME            = "postgres"
+  }
+
+  name = "banking_connection"
+
+  physical_connection_requirements {
+    availability_zone      = "us-east-1e"
+    security_group_id_list = ["sg-0b8fceff13339705f"]
+    subnet_id              = "subnet-0ff945865ca5a1cda"
+  }
+}
+
+resource "aws_glue_crawler" "banking_crawler" {
+  database_name = "banking_group"
+  name          = "banking_crawler"
+  role          = "arn:aws:iam::370310570296:role/big-boss-man"
+
+  jdbc_target {
+    connection_name = aws_glue_connection.banking_connection.name
+    path            = "finance/%"
+  }
+}
+
 
 ##################
 # Glue Job       #
 ##################
+resource "aws_glue_job" "banking_job" {
+  name              = "banking_job"
+  role_arn          = "arn:aws:iam::370310570296:role/big-boss-man"
+  glue_version      = "3.0"
+  worker_type       = "G.1X"
+  number_of_workers = 3
 
+  command {
+    script_location = "s3://aws-glue-assets-370310570296-us-east-1/scripts/test-delete.py"
+  }
+
+  default_arguments = {
+    "--additional-python-modules"        = "s3-concat"
+    "--job-bookmark-option"              = "job-bookmark-enable"
+    "--enable-continuous-cloudwatch-log" = "true"
+  }
+}
+
+############################
+# S3 Bucket with SSE       #
+############################
+resource "aws_s3_bucket" "banking_output" {
+  bucket = "my-edokemwa-tf-test-bucket"
+
+  tags = {
+    Name        = "Banking output bucket to share"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "banking_output_encrypt" {
+  bucket = aws_s3_bucket.banking_output.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+
+##################
+# SFTP server    #
+##################
+resource "aws_instance" "sftp_server" {
+  ami           = "ami-05d15be56f7c1ea2f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SftpS3MountServer"
+  }
+}
+
+
+#########################
+# SNS Subscription      #
+#########################
+resource "aws_sns_topic" "banking_job_update" {
+  name = "banking-job-topic"
+}
+
+resource "aws_sns_topic_subscription" "banking_job_failure" {
+  topic_arn = aws_sns_topic.banking_job_update.arn
+  protocol  = "email"
+  endpoint  = "data-support@mybigbank.co.za"
+}
+
+#############################################################
+# STEP Functions for orchestration and notification         #
+#############################################################
+resource "aws_sfn_state_machine" "banking_state_machine" {
+  name     = "banking-state-machine"
+  role_arn = "arn:aws:iam::370310570296:role/service-role/StepFunctions-test-delete-glue-role-d6b9a3df"
+
+  definition = <<EOF
+{
+  "Comment": "Workflow to run Glue Job",
+  "StartAt": "Glue StartJobRun",
+  "States": {
+    "Glue StartJobRun": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::glue:startJobRun.sync",
+      "Parameters": {
+        "JobName": "test-delete"
+      },
+      "Comment": "Run Glue Job",
+      "Next": "Choice"
+    },
+    "Choice": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Not": {
+            "Variable": "$.JobRunState",
+            "StringEquals": "SUCCEEDED"
+          },
+          "Next": "Publish"
+        }
+      ],
+      "Default": "Pass"
+    },
+    "Publish": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::sns:publish",
+      "Parameters": {
+        "Message.$": "$",
+        "TopicArn": "arn:aws:sns:us-east-1:370310570296:amplify_codecommit_topic"
+      },
+      "End": true
+    },
+    "Pass": {
+      "Type": "Pass",
+      "End": true
+    }
+  }
+}
+EOF
+}
