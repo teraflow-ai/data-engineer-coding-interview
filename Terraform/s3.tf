5178e06711b2b24d22c966b42b6836f913c1719e
gizelle <theron.gizelle@gmail.com>
2023-04-07 22:54:27 +0200

    Terraform: Add comment

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,6 +1,8 @@
+######################################
+# Upload ETL script to S3 location
+######################################
 resource "aws_s3_object" "upload_etl_script" {
   bucket = "aws-glue-assets-953443259787-eu-west-1"
   key    = var.glue_etl_script_obj_key
   source = "${path.cwd}/../Glue/etl.py"
-
 }
