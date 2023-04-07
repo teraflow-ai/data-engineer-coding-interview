737d33cb541227c7fe187836639ee5776a1c9ad4
gizelle <theron.gizelle@gmail.com>
2023-04-07 22:34:42 +0200

    Terraform: Fix comment

--- Terraform/local.tf
+++ Terraform/local.tf
@@ -5,7 +5,7 @@ locals {
   rds_username   = jsondecode(data.aws_secretsmanager_secret_version.banks_credentials.secret_string)["username"]
   rds_pwd        = jsondecode(data.aws_secretsmanager_secret_version.banks_credentials.secret_string)["password"]
 
-  # Find the private Subnet linked to the AZ where the DB instance is running from
+  # Find the Subnet linked to the AZ where the DB instance is running from
   rds_subnet_ids     = data.aws_subnet.vpc_subnet
   rds_az_subnets     = [for v in local.rds_subnet_ids : v if v.availability_zone == local.rds_cluster_az && v.map_public_ip_on_launch == true]
   rds_primary_subnet = local.rds_az_subnets[0].id
